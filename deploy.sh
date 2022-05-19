bdfb1577b238eca2991540ed4c7df22e311160fb
laslopaul <96680549+laslopaul@users.noreply.github.com>
2022-05-20 02:32:07 +0300

    Update deploy.sh from staging

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,14 @@
 if [[ $1 = "staging" ]]
 then
     PORT=5000
+    DOCKER_TAG=$2-$1
 else
     PORT=8000
+    DOCKER_TAG=latest
 fi
 
 echo "Starting to deploy docker image.."
 DOCKER_IMAGE=laslopaul/flask-hello
-docker pull $DOCKER_IMAGE
+docker pull $DOCKER_IMAGE:$DOCKER_TAG
 docker ps -q --filter ancestor=$DOCKER_IMAGE | xargs -r docker stop
 docker run -dp $PORT:$PORT $DOCKER_IMAGE
