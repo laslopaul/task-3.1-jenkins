c330fbbdee64f6f57a9bceb67fdeb98e5a153907
laslopaul <96680549+laslopaul@users.noreply.github.com>
2022-05-20 20:20:54 +0300

    Add --rm parameter

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ DOCKER_TAG=$1-staging
 echo "Starting to deploy docker image.."
 docker pull $DOCKER_IMAGE:$DOCKER_TAG
 docker ps -q --filter name=$DOCKER_CONTAINER | xargs -r docker stop
-docker run --name $DOCKER_CONTAINER -dp $PORT:$PORT $DOCKER_IMAGE:$DOCKER_TAG
+docker run --rm --name $DOCKER_CONTAINER -dp $PORT:$PORT $DOCKER_IMAGE:$DOCKER_TAG
